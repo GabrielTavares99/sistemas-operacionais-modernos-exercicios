5652d77a90f28ed42bb532b57177cdf643e8d33b
Gabriel Tavares <gabriel.tavares99@hotmail.com>
2018-03-06 20:21:55 -0300

    Método pegar propriedades do sistema operacional

--- src/main/java/processos/controller/ProcController.java
+++ src/main/java/processos/controller/ProcController.java
@@ -1,4 +1,27 @@
 package processos.controller;
 
+import java.util.Properties;
+import java.util.Set;
+
 public class ProcController {
+
+    public ProcController() {
+//        O super pega alguns métodos da classe object
+        super();
+    }
+
+    public String os() {
+        return System.getProperty("os.name");
+    }
+
+    public void propriedades() {
+        Properties properties = System.getProperties();
+        Set<Object> chaves = properties.keySet();
+        for (Object key: chaves) {
+            System.out.print(key);
+            System.out.print("=");
+            System.out.println(System.getProperty(key.toString()));
+        }
+    }
+
 }
--- src/main/java/processos/view/Principal.java
+++ src/main/java/processos/view/Principal.java
@@ -1,7 +1,15 @@
 package processos.view;
 
+import processos.controller.ProcController;
+
 public class Principal {
     public static void main(String[] args) {
+
+        ProcController procController = new ProcController();
+
+        System.out.println(procController.os());
+
+        procController.propriedades();
 
     }
 }
