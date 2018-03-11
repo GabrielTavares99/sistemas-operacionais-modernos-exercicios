5612f40b22d2224fb64dfc2057e8d1d1870f837d
Gabriel Tavares <gabriel.tavares99@hotmail.com>
2018-03-11 01:13:50 -0300

    Exercício 1 - Criando menu principal

--- src/main/java/processos/view/MainRedesController.java
+++ src/main/java/processos/view/MainRedesController.java
@@ -2,6 +2,9 @@ package processos.view;
 
 import processos.controller.RedesController;
 
+import java.util.Scanner;
+import java.util.StringJoiner;
+
 public class MainRedesController {
     public static void main(String[] args) {
 
@@ -9,13 +12,38 @@ public class MainRedesController {
 
         String osName = System.getProperty("os.name");
 
-        System.out.print("Adaptadores de rede:");
-        System.out.println(redesController.ip(osName));
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println();
+        int opcao = 0;
 
-        System.out.println("Ping médio: ");
-        System.out.println(redesController.ping(osName));
+        while (opcao != 99){
+            StringJoiner menu = new StringJoiner("\n");
+            menu.add("Bem Vindo à Central de Redes:");
+            menu.add("1 - Ver adaptadores de rede");
+            menu.add("2 - Ver ping da rede");
+            menu.add("99 - Sair");
+            System.out.println(menu.toString());
+            opcao = scanner.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.print("Adaptadores de rede:");
+                    System.out.println(redesController.ip(osName));
+                    break;
+                case 2:
+                    System.out.println("Ping médio: ");
+                    System.out.println(redesController.ping(osName));
+                    break;
+                case 99:
+                    System.out.println("Bye o/");
+                    System.exit(0);
+                    break;
+                default:
+                    System.err.println("Opção inválida");
+                    break;
+            }
+
+            System.out.println();
+        }
 
 
     }
