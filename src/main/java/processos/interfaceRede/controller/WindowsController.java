a424f8b0ff1e1d22be0fa3499f1dd6593ee9e410
Gabriel Tavares <gabriel.silva@onebox.net.br>
2018-03-12 16:07:40 -0300

    Exercício 1b - Windows

--- src/main/java/processos/interfaceRede/controller/WindowsController.java
+++ src/main/java/processos/interfaceRede/controller/WindowsController.java
@@ -1,5 +1,10 @@
 package processos.interfaceRede.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class WindowsController implements ISoController {
 
 
@@ -10,6 +15,29 @@ public class WindowsController implements ISoController {
 
     @Override
     public double ping() {
+
+        String comandoPing = "ping -n 10 www.google.com";
+        StringBuilder pingString = new StringBuilder();
+        try {
+            Process processo = Runtime.getRuntime().exec(comandoPing);
+            InputStream stream = processo.getInputStream();
+            InputStreamReader reader = new InputStreamReader(stream);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String linha = bufferedReader.readLine();
+            while (linha != null){
+                System.out.println(linha);
+                pingString.append(linha);
+                bufferedReader.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int inicio = pingString.indexOf("Média");
+        int fim = inicio+14;
+        String sub = pingString.toString().substring(inicio, fim);
+        System.out.println(sub);
         return 0;
     }
 }
