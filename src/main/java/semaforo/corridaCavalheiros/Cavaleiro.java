d2ad76bded12f9452f80cc322b0a1d02aff37a2e
Gabriel Tavares <gabriel.tavares99@hotmail.com>
2018-03-30 09:02:59 -0300

    Finalizando corrida de cavaleiros

--- src/main/java/semaforo/corridaCavalheiros/Cavaleiro.java
+++ src/main/java/semaforo/corridaCavalheiros/Cavaleiro.java
@@ -3,15 +3,21 @@ package semaforo.corridaCavalheiros;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
+import static semaforo.corridaCavalheiros.Principal.PORTAS_DISPONIVEIS;
+
 public class Cavaleiro extends Thread {
 
     static boolean TOCHA = true;
+    static boolean PEDRA_BRILHANTE = true;
 
     private Semaphore semaforo;
     private int id_num;
     int bonus = 0;
     private int posicao = 0;
     Random random = new Random();
+
+    String encontrouSaida = "encontrou a saída";
+    String foiDevorado = "foi devorado pelo monstro";
 
     public Cavaleiro(Semaphore semaforo, int id_num) {
         this.semaforo = semaforo;
@@ -27,15 +33,43 @@ public class Cavaleiro extends Thread {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            int deslocamento = (random.nextInt(2) + 2)+bonus;
+            int deslocamento = (random.nextInt(2) + 2) + bonus;
             posicao += deslocamento;
             System.out.println(String.format("Cavaleiro #%d, andou %d,  está na posição %d", id_num, deslocamento, posicao));
-            if (posicao >= 500 && TOCHA) {
-                System.out.println(String.format("Cavaleiro #%d, pegou a tocha!!!", id_num));
-                TOCHA = false;
-                bonus+=2;
+
+            try {
+                semaforo.acquire();
+                if (posicao >= 500 && TOCHA) {
+                    System.out.println(String.format("Cavaleiro #%d, pegou a tocha!!!", id_num));
+                    TOCHA = false;
+                    bonus += 2;
+                }
+                semaforo.release();
+                semaforo.acquire();
+                if (posicao >= 1500 && PEDRA_BRILHANTE) {
+                    System.out.println(String.format("Cavaleiro #%d, pegou a pedra brilhante!!!", id_num));
+                    PEDRA_BRILHANTE = false;
+                    bonus += 2;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaforo.release();
             }
         }
+        System.out.println(String.format("Cavaleiro #%d está na escolha da porta!", id_num));
+        try {
+            semaforo.acquire();
+            int escolhaPorta = random.nextInt(PORTAS_DISPONIVEIS.size());
+            String mensagemDestino = PORTAS_DISPONIVEIS.get(escolhaPorta) ? encontrouSaida : foiDevorado;
+            System.out.println(String.format("Cavaleiro #%d ", id_num).concat(mensagemDestino));
+            PORTAS_DISPONIVEIS.remove(escolhaPorta);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaforo.release();
+        }
+
 
     }
 }
--- src/main/java/semaforo/corridaCavalheiros/Principal.java
+++ src/main/java/semaforo/corridaCavalheiros/Principal.java
@@ -1,15 +1,26 @@
 package semaforo.corridaCavalheiros;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.Semaphore;
 
 public class Principal {
 
+    static List<Boolean> PORTAS_DISPONIVEIS = new ArrayList<>();
 
     public static void main(String[] args) {
 
         Semaphore semaforo = new Semaphore(1);
 
-        for (int i = 1; i <= 4 ; i++) {
+        Random random = new Random();
+
+        Boolean[] portas = new Boolean[]{false, false, false, false};
+        portas[random.nextInt(portas.length)] = true;
+        PORTAS_DISPONIVEIS.addAll(Arrays.asList(portas));
+
+        for (int i = 1; i <= 4; i++) {
             new Cavaleiro(semaforo, i).start();
         }
 
